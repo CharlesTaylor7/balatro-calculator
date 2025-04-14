6d0cd04006383149055643a6116bc02666063640
Charles Taylor <charlestaylor95@gmail.com>
2025-04-13 22:14:59 -0400

    Install Shacdn

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
-import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import path from "node:path";
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "/balatro-calculator/",
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   server: {
     port: 3000,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
 });
