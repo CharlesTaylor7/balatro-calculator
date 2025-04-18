60ff55367449e1c08c0fa5c7d6ebbe594f643f3f
Charles Taylor <charlestaylor95@gmail.com>
2025-04-17 23:02:33 -0400

    Add theme toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { JokerList } from "@/JokerList";
 import { Rounds } from "@/Rounds";
 import { HandInfo } from "@/HandInfo";
+import { ModeToggle } from "./components/mode-toggle";
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <div className="flex items-start justify-between gap-2">
         <JokerList />
         <Rounds />
+        <ModeToggle />
       </div>
       <HandInfo />
     </>
