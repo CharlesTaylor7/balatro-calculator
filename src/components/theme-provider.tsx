a8660647e3529de060c30ca02650d8630d57bf99
Charles Taylor <charlestaylor95@gmail.com>
2025-04-24 16:08:32 -0400

    Fix fast refresh

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -1,26 +1,12 @@
 // https://ui.shadcn.com/docs/dark-mode/vite
 import { createContext, useContext, useEffect, useState } from "react";
-
-type Theme = "dark" | "light" | "system";
+import { ThemeProviderContext, Theme } from "@/hooks/theme-context";
 
 type ThemeProviderProps = {
   children: React.ReactNode;
   defaultTheme?: Theme;
   storageKey?: string;
 };
-
-type ThemeProviderState = {
-  theme: Theme;
-  setTheme: (theme: Theme) => void;
-};
-
-const initialState: ThemeProviderState = {
-  theme: "system",
-  setTheme: () => null,
-};
-
-const ThemeProviderContext = createContext<ThemeProviderState>(initialState);
-
 export function ThemeProvider({
   children,
   defaultTheme = "system",
@@ -64,11 +50,3 @@ export function ThemeProvider({
   );
 }
 
-export const useTheme = () => {
-  const context = useContext(ThemeProviderContext);
-
-  if (context === undefined)
-    throw new Error("useTheme must be used within a ThemeProvider");
-
-  return context;
-};
